1926c9e6484a1523ce547b42e40ce7ac3c90323f
16JunYS <popo3176junys@gmail.com>
2019-04-08 13:20:01 +0200

    upload the Image from Gallery or Camera

--- app/src/main/java/com/example/fhv2ndhandprod/UploadListProductsActivity.java
+++ app/src/main/java/com/example/fhv2ndhandprod/UploadListProductsActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +34,7 @@ public class UploadListProductsActivity extends ArrayAdapter<Product> {
         Product product = sellProducts.get(position);
         //get the inflater and inflate the XML layout for each item
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.uploadproductgui, null);
+        View view = inflater.inflate(R.layout.activity_uploadproduct, null);
 
         //Changed TextView to EditText, new GUI
         EditText description = (EditText) view.findViewById(R.id.productNameEditText);
--- app/src/main/java/com/example/fhv2ndhandprod/UploadProductActivity.java
+++ app/src/main/java/com/example/fhv2ndhandprod/UploadProductActivity.java
@@ -1,23 +1,36 @@
 package com.example.fhv2ndhandprod;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class UploadProductActivity extends AppCompatActivity{
     private EditText productNameEditText, productDescriptionEditText, productPriceEditText;
+    private String ImagePath;
     private ImageView productImageView;
     private Button uploadButton;
 
     private Product newProduct;
 
+    private int GALLERY = 1, CAMERA = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.uploadproductgui);
+        setContentView(R.layout.activity_uploadproduct);
         productNameEditText = (EditText) findViewById(R.id.productNameEditText);
         productDescriptionEditText = (EditText) findViewById(R.id.productDescriptionEditText);
         productImageView = (ImageView) findViewById(R.id.productImageView);
@@ -32,6 +45,9 @@ public class UploadProductActivity extends AppCompatActivity{
         });
     }
 
+    public void upload_image(View view) {
+
+    }
     private void createNewProduct() {
         newProduct.setName(productNameEditText.getText().toString());
         newProduct.setDescription(productDescriptionEditText.getText().toString());
@@ -42,7 +58,51 @@ public class UploadProductActivity extends AppCompatActivity{
             //save into db
         }
     }
+    public void selectPhotoFromGallery(){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+    public void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //get Image data & set it in ImageView
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == this.RESULT_CANCELED) return;
+        if(requestCode == GALLERY) { //uploaded from Gallery
+            if(data != null) {
+                Uri ProdUri = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                /*
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), ProdUri);
+                    ImagePath = saveImage(bitmap);
+                    Toast.makeText(UploadProductActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+                    productImageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(UploadProductActivity.this, "Failed.", Toast.LENGTH_SHORT).show();
+                }
+                */
+            }
+        }
+        else if(requestCode == CAMERA) { //uploaded from Camera
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+
+            productImageView.setImageBitmap(thumbnail);
+            saveImage(thumbnail);
+            Toast.makeText(UploadProductActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    private String saveImage(Bitmap bitmap) {
+
+        return "";
+    }
     private boolean validateNewProduct(Product product) {
         if(product.getId() == 0) {
             return false;
@@ -61,4 +121,8 @@ public class UploadProductActivity extends AppCompatActivity{
         }
         return true;
     }
+
+
+
+
 }
