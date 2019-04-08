ae346ae910d066c96b2a7f8b4193cdf27e1374de
16JunYS <popo3176junys@gmail.com>
2019-04-08 11:06:35 +0200

    translate into Deutsch _v1 :login activity, register activity

--- app/src/main/java/com/example/fhv2ndhandprod/MainActivity.java
+++ app/src/main/java/com/example/fhv2ndhandprod/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.res.Configuration; //for translator
+import java.util.Locale; //for translator
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Locale locale = new Locale("de");
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
     }
     public void TEMP_register(View view) {
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
